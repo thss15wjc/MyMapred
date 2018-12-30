6d608cc4d2d2ed912ee98465ebf16a4297dfc0ed
thss15wjc <740834138@qq.com>
2018-12-30 14:40:30 +0800

    mapcontext

--- src/RecordReader.java
+++ src/RecordReader.java
@@ -10,59 +10,12 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.util.LineReader;
 
-public class RecordReader <KEYIN, VALUEIN> {
-	public class LineRecordReader extends RecordReader<LongWritable, Text> {
-		private long start;
-		private long pos;
-		private long end;
-		private LongWritable key = null;
-	    private Text value = null;
-	    private FSDataInputStream fin = null;
-	    private LineReader reader = null;
-		public void initialize(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
-			FileSplit fileSplit = (FileSplit) split;
-			start = fileSplit.getStart();
-			end = start + fileSplit.getLength();
-			Configuration conf = context.getConfiguration();
-			Path path = fileSplit.getPath();
-			FileSystem fileSystem = path.getFileSystem(conf);
-			fin = fileSystem.open(path);
-			fin.seek(start);
-			reader = new LineReader(fin);
-			pos = start;
-		}
-		public boolean nextKeyValue() throws IOException, InterruptedException {
-			if(key == null){
-                key = new LongWritable();
-            }
-            key.set(pos);
-            if(value == null){
-                value = new Text();
-            }
-            int newSize = 0;
-            if (pos < end) {
-            	newSize = reader.readLine(value);
-            	pos += newSize;
-            }
-            if(newSize==0){
-                return false;
-            } else return true;
-
-		}
-		public LongWritable getCurrentKey() throws IOException, InterruptedException {
-			return key;
-		}
-		public Text getCurrentValue() throws IOException, InterruptedException {
-			return value;
-		}
-		public float getProgress() throws IOException, InterruptedException {
-			if (start ==end) 
-				return 0.0f;
-			else 
-				return Math.min(1.0f, (pos -start) / (float)(end -start));
-		}
-		public void close() throws IOException {
-	        fin.close();
-	    }
-	}
+public abstract class RecordReader <KEYIN, VALUEIN> {
+	public abstract void initialize(InputSplit split, JobContext context) throws IOException, InterruptedException;
+	public abstract boolean nextKeyValue() throws IOException, InterruptedException;
+	public abstract KEYIN getCurrentKey() throws IOException, InterruptedException;
+	public abstract VALUEIN getCurrentValue() throws IOException, InterruptedException;
+	public abstract float getProgress() throws IOException, InterruptedException;
+	public abstract void close() throws IOException;
+	
 }
